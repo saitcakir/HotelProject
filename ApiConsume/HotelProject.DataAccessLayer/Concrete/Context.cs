cd49fb86e4c0d987cd7a84b001c2d15abaac5b88
saitcakir <sait25@windowslive.com>
2023-07-14 16:12:17 +0300

    Data Access Layer işlemleri yapıldı.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -8,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.Concrete
 {
-    public class Context:DbContext
+    public class Context : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           optionsBuilder.UseSqlServer  ("server=DESKTOP-AKNTGAT;initial catalog=HotelApidb;integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-AKNTGAT;initial catalog=HotelApidb;integrated security=true");
         }
 
-        public DbSet<Room> Rooms { get; set; }  
-        public DbSet<Service> Services { get; set; }  
-        public DbSet<Staff> Staffs { get; set; }  
-        public DbSet<Subscribe> Subscribes { get; set; }  
-        public DbSet<Testimonial> Testimonials { get; set; }  
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
